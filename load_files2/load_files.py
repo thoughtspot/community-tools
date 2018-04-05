9c6a75e7c43948a831167abc2872e1806456a451
Bill Back <bill.back@thoughtspot.com>
2018-04-05 13:24:08 -0700

    First working version.  Cleanup and logging to be added.

--- load_files2/load_files.py
+++ load_files2/load_files.py
@@ -26,6 +26,8 @@ from os import listdir
 from os.path import isfile, isdir, join
 import json
 import ntpath
+import subprocess
+from multiprocessing import Pool
 
 
 def eprint(*args, **kwargs):
@@ -41,123 +43,6 @@ def json_dump(j):
     return json.dumps(j, sort_keys=True, indent=4, separators=(",", ": "))
 
 
-class TSFileLoader(object):
-    """
-    Loads a file into ThoughtSpot using sub-process calls to tsload.
-    """
-
-    def __init__(self, settings):
-        """
-        Creates a new file loader with the given settings.
-        :param settings: A JSON object with the settings to use for loading files.
-        :type settings: dict
-        """
-        self.settings = settings
-        self.cmd = self.get_base_tsload_command()  # populate here, so the object can be reused without redoing completely.
-        print(self.cmd)
-
-    def get_base_tsload_command(self):
-        """
-        Calculates the tsload command based on the filename and the flags.
-        :return: The base command to execute with the filename and table_name as arguments.
-        :rtype: str
-        """
-        cmd = "cat {filename} | tsload --target_table {table_name}"
-        for key in self.settings:
-            if key.startswith("tsload"):
-                flag = key.split(".")[
-                    1
-                ]  # get the key after the "tsload." indicator.
-                value = str(self.settings[key])
-                if value == "true":
-                    cmd += " --%s" % flag  # indicates a switch flag.  Hopefully not a valid value for something else.
-                elif value == "false":
-                    pass  # don't add false flags.
-                else:
-                    cmd += ' --%s "%s"' % (flag, value)
-
-        print(cmd)
-        return cmd
-
-    def load_file(self, filepath):
-        """
-        Loads an individual file using tsload.
-        :param filepath: The path to the file to load.
-        :type filepath: str
-        :return: Results of the load process as a string.
-        :rtype: str
-        """
-        print("Loading %s...." % filepath)
-        # always strip the path off the filepath.
-        head, tail = ntpath.split(filepath)
-        table_name = tail or ntpath.basename(head)
-        # table names are the name of the file, minus:
-        # - .extension
-        # - _incremental or _full
-        # - ?? do we somehow allow timestamps?  Easier if not.  Maybe add as a future enhancement.  One option would
-        #      be to just to provide a pattern, or maybe a strip flag for _ and -.
-        #   -- solution - add a table name terminator that we will ignore everything after that.
-
-        # TODO add check for empty target flag.  This can override the default.  Support _incremental and _full
-
-        cmd = self.cmd.format(
-            filename=filepath, table_name=table_name
-        )  # TODO add format for file and table names.
-        print(cmd)
-        return "success"
-
-
-class TSLoadManager(object):
-    """
-    Manages the loading of one or more files into ThoughtSpot using tsload.  The files can be loaded into parallel
-    up to the number of parallel loads specified in the settings.
-    """
-
-    def __init__(self, settings):
-        """
-        Creates a load manager to load files using tsload.  An error will be thrown if there are missing mandatory
-        parameters.
-        :param settings: A JSON object with the settings to use for loading.
-        :type settings: dict
-        """
-        self.settings = settings
-
-        self.root_directory = settings.get("root_directory", None)
-        if self.root_directory is None or not isdir(self.root_directory):
-            raise Exception(
-                "The root_directory %s doesn't exist or it not a valid directory."
-                % self.root_directory
-            )
-
-        self.data_directory = settings.get(
-            "data_directory", self.root_directory + "/data"
-        )
-        if not isdir(self.data_directory):
-            raise Exception(
-                "The data_directory %s doesn't exist or it not a valid directory."
-                % self.root_directory
-            )
-
-        self.max_simultaneous_loads = int(
-            settings.get("max_simultaneous_loads", "1")
-        )  # might throw an exception.
-        pass
-
-    def load_files(self):
-        """
-        Manages the loading of files.
-        """
-        for f in [
-            join(self.data_directory, f)
-            for f in listdir(self.data_directory)
-            if isfile(join(self.data_directory, f))
-        ]:
-            tsloader = TSFileLoader(
-                self.settings
-            )  # see if I can create a pool and reuse if worth it.
-            print(tsloader.load_file(f))
-
-
 def main():
     """
     Loads files using tsload based on the settings in the settings file.
@@ -165,8 +50,9 @@ def main():
     args = parse_args()
     if valid_args(args):
         settings = read_settings(args.filename)
-        load_manager = TSLoadManager(settings)
-        load_manager.load_files()
+        # root_directory, data_directory = get_directory_settings(settings)
+        base_cmd = create_base_command(settings)
+        load_files(settings, base_cmd)
 
 
 def parse_args():
@@ -211,6 +97,162 @@ def read_settings(settings_filename):
         print(json_dump(settings))
 
     return settings
+
+
+def create_base_command(settings):
+    """
+    Creates the base tsload command based on the settings.
+    :param settings:  Dictionary of settings for tsload.
+    :type settings: dict
+    :return:  A string to use for the command containing two parameters:  {file_path} and {table_name}
+    :rtype: str
+    """
+    cmd = "cat {file_path} | tsload --target_table {table_name}"
+    for key in settings:
+        if key.startswith("tsload"):
+            flag = key.split(".")[
+                1
+            ]  # get the key after the "tsload." indicator.
+            value = str(settings[key])
+            if value == "true":
+                cmd += " --%s" % flag  # indicates a switch flag.  Hopefully not a valid value for something else.
+            elif value == "false":
+                pass  # don't add false flags.
+            else:
+                cmd += ' --%s "%s"' % (flag, value)
+
+    return cmd
+
+
+def load_files(settings, base_cmd):
+    """
+    :param settings:  Dictionary of settings for tsload.
+    :type settings: dict
+    :param base_cmd:  The base command to use for tsload after formatting.
+    :type base_cmd: str
+    """
+
+    root_directory, data_directory = get_directory_settings(settings)
+    file_extension = settings.get(
+        "filename_extension", ".csv"
+    )  # default to .csv if none provided.
+
+    max_simultaneous_loads = int(
+        settings.get("max_simultaneous_loads", 1)
+    )  # might throw an exception if not an int.
+    pool = Pool(processes=max_simultaneous_loads)
+
+    # get a list of commands to run in parallel.
+    commands = []
+    for f in [
+        join(data_directory, f)
+        for f in listdir(data_directory)
+        if isfile(join(data_directory, f)) and f.endswith(file_extension)
+    ]:
+        commands.append(update_base_cmd_for_file(settings, base_cmd, f))
+
+    results = [pool.apply_async(load_a_file, (cmd,)) for cmd in commands]
+    for res in results:
+        print(res.get())
+
+
+def get_directory_settings(settings):
+    """
+    Verifies (as much as possible) that the settings are valid.
+    :param settings:  Dictionary of settings for tsload.
+    :type settings: dict
+    :returns: The root directory and data directory.
+    :rtype: (str, str)
+    """
+    root_directory = settings.get("root_directory", None)
+    if not root_directory or not isdir(root_directory):
+        raise IOError(
+            "The root_directory %s doesn't exist or it not a valid directory."
+            % root_directory
+        )
+
+    data_directory = settings.get("data_directory", root_directory + "/data")
+    if not isdir(data_directory):
+        raise IOError(
+            "The data_directory %s doesn't exist or it not a valid directory."
+            % root_directory
+        )
+
+    return root_directory, data_directory
+
+
+def update_base_cmd_for_file(settings, base_cmd, file_path):
+    """
+    Updates the base_cmd based on the file name.
+    :param settings: Settings from the user.
+    :type settings: dict
+    :param base_cmd: The base command that will need to be modified for the file.
+    :type base_cmd: str
+    :param file_path: The path to the file to load.
+    :type file_path: str
+    :return: The adjusted command tailored for the file.
+    :rtype: str
+    """
+
+    # always strip the path off the file_path.
+    head, tail = ntpath.split(file_path)
+    table_name = tail or ntpath.basename(head)
+
+    # table names are the name of the file, minus:
+    # - .extension
+    # - _incremental or _full
+    # - ?? do we somehow allow timestamps?  Easier if not.  Maybe add as a future enhancement.  One option would
+    #      be to just to provide a pattern, or maybe a strip flag for _ and -.
+
+    # remove the extension.
+    table_name = table_name.split(settings.get("filename_extension", ""))[0]
+
+    # take off . in case it's not included in the extension.  That would be an invalid table name.
+    if table_name.endswith("."):
+        table_name = table_name[:-1]
+
+    # handle _full and _incremental.  MUST be last thing before the extension.
+    empty_target = settings.get("settings.empty_target", "")
+    if table_name.endswith("_full"):
+        empty_target = "--empty_target"
+        table_name = table_name[:-len("_full")]
+    elif table_name.endswith("_incremental"):
+        empty_target = ""
+        table_name = table_name[:-len("_incremental")]
+
+    cmd = base_cmd.format(
+        file_path=file_path, table_name=table_name
+    )  # TODO add format for file and table names.
+
+    cmd = cmd.replace(
+        "--empty_target", empty_target
+    )  # simply replace the default if overridden.
+
+    return cmd
+
+
+def load_a_file(cmd):
+    """
+    Loads an individual file using tsload.
+    :param cmd: The base command to use for tsload.  Gets modified for file and table name.
+    :type cmd: str
+    :return:  The output as a string.
+    :rtype: str
+    """
+
+    # fork out to the command and capture the results.
+    print(cmd)
+    try:
+        output = subprocess.check_output(
+            cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True
+        )
+    except subprocess.CalledProcessError as cpe:
+        # print ("Status : Fail", cpe.returncode, cpe.output)
+        output = cpe.output
+    # else:
+    #     print("Output: \n{}".format(output))
+
+    return output
 
 
 if __name__ == "__main__":
