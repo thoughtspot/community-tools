b8c4d52b6c180cf512fc165bfdc559c17affb155
Rajesh Ravindran <rajesh.ravindran@thoughtspot.com>
2018-11-13 18:18:13 +0530

    updated test code

--- test/UserManagement/test_delUser.py
+++ test/UserManagement/test_delUser.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from UserManagement import delUser
 
+"""NEED TO ADD MORE TEST CASES"""
 class TestDelUser(TestCase):
     def test_negative(self):
         #s = delUser()
         self.assert_
+
