f4d616f9151d8d431bf8d62cbdd357a240bc22d3
Rajesh Ravindran <rajesh.ravindran@thoughtspot.com>
2018-11-13 18:12:15 +0530

    added test module

---
+++ test/UserManagement/test_delUser.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from UserManagement import delUser
+
+class TestDelUser(TestCase):
+    def test_negative(self):
+        #s = delUser()
+        self.assert_
+       # self.fail('Purposefully failing')
