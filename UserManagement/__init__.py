ad9276874ee08b6e7613322c5d17a6a52282a76f
Rajesh Ravindran <rajesh.ravindran@thoughtspot.com>
2019-01-10 12:32:09 +0530

    updated

--- UserManagement/__init__.py
+++
@@ -1,4 +0,0 @@
-from delete_ugs import delUser
-from get_users import getUsers
-from tsUserGroupApi import *
-
--- test/UserManagement/test_delUser.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from UserManagement import delUser
-
-class TestDelUser(TestCase):
-    def test_negative(self):
-        #s = delUser()
-        self.assert_
